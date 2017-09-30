042d2ffab0f0be7fd8f77b01ab97ac2b54e37dea
Alex Sicart Ramos <alex@sharge.io>
2017-09-30 14:48:59 +0200

    feat: node working

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Files from './files';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="row">
+        <div class="one-half column">
+          <Files></Files>
+        </div>
+        <div class="one-half column">
+          <h1> Hello </h1>
+        </div>
       </div>
     );
   }
